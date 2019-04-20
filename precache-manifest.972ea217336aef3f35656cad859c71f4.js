6b3372da61e8f744a9248cb879be06ef7bea0714
Shubhojeet Cl <shubhojeetcl@gmail.com>
2019-04-20 15:13:26 +0530

    timer

---
+++ precache-manifest.972ea217336aef3f35656cad859c71f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "66efec37624bb3344ff1",
+    "url": "/static/js/main.55ec0362.chunk.js"
+  },
+  {
+    "revision": "9947867f0517f3cf17f9",
+    "url": "/static/js/2.001598ca.chunk.js"
+  },
+  {
+    "revision": "66efec37624bb3344ff1",
+    "url": "/static/css/main.5d3074c5.chunk.css"
+  },
+  {
+    "revision": "48c499f500434f60ada44b86e1ef4760",
+    "url": "/index.html"
+  }
+];
